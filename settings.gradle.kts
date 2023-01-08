5668b44c9456b46d276ad6e234508ba275cd86c0
İlyas ŞAVLİ <ilyas.savli47@gmail.com>
2023-01-08 23:19:29 +0300

    Added libs.versions.toml file to enable version catalogs
    Added libs.versions.toml file to enable version catalogs
    
    Added 3 type of version catalog TOML files :100:
    1 - AndroidX
    2 - Stack
    3 - RedMadRobot

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,21 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
+    versionCatalogs {
+        create("anx") {
+            from(
+                files(
+                    "versions-stack/libs.versions.toml"
+                )
+            )
+        }
+        /*create("rmr") {
+            from(files("versions-rmr/others.versions.toml"))
+        }
+        create("stack") {
+            from(files("versions-stack/libs.versions.toml"))
+        }*/
+    }
 }
 rootProject.name = "AndroidBase"
 include(":app")
