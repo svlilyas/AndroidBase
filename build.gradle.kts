b7a5c66cf1f406b80eadc87ecad5dcaef2095ba6
İlyas ŞAVLİ <ilyas.savli47@gmail.com>
2023-01-08 22:29:59 +0300

    Converting Gradle files from Groovy to Gradle DSL
    Converting Gradle files from Groovy to Gradle DSL
    
    * :100: Added DataBinding Feature
    * :+1:  JDK changed to JAVA 19

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+    }
+}
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "AndroidBase"
-include ':app'
+include(":app")
